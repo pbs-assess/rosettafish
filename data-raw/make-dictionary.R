257f447c68aa10858dad229695e43c385a567176
Sean Anderson <sean@seananderson.ca>
2018-11-30 15:08:05 -0800

    row.names <- NULL

--- data-raw/make-dictionary.R
+++ data-raw/make-dictionary.R
@@ -2,7 +2,7 @@ rosetta_terms <- as.data.frame(readr::read_csv("inst/extdata/terms.csv"))
 
 language <- "english"
 rosetta_terms <- rosetta_terms[order(rosetta_terms[[language]]), , drop = FALSE]
-
+row.names(rosetta_terms) <- NULL
 readr::write_csv(rosetta_terms, "inst/extdata/terms.csv")
 
 usethis::use_data(rosetta_terms, overwrite = TRUE, internal = TRUE)

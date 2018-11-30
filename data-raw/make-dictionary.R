4bd1b110f83b47e15512a549dc9e218c0bf18fa8
Sean Anderson <sean@seananderson.ca>
2018-11-30 10:30:26 -0800

    Move sortfish into data-raw (when it will be used); avoid dependencies

--- data-raw/make-dictionary.R
+++ data-raw/make-dictionary.R
@@ -1,12 +1,8 @@
 rosetta_terms <- read.csv("inst/extdata/terms.csv", stringsAsFactors = FALSE, 
   strip.white = TRUE, fileEncoding = "utf8")
 
-# https://stackoverflow.com/questions/18837855/making-non-ascii-data-suitable-for-cran
-# Encoding(rosetta_terms$english) <- "latin1"
-# rosetta_terms$english <- iconv(rosetta_terms$english, "latin1", "UTF-8")
-# 
-# Encoding(rosetta_terms$french) <- "latin1"
-# rosetta_terms$french <- iconv(rosetta_terms$french, "latin1", "UTF-8")
+language <- "english"
+rosetta_terms <- rosetta_terms[order(rosetta_terms[[language]]), , drop = FALSE]
 
 usethis::use_data(rosetta_terms, overwrite = TRUE, internal = TRUE)
 

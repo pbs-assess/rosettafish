0496dc0fb7560be13589b5b688598fd1946dfc36
Sean Anderson <sean@seananderson.ca>
2018-11-30 10:31:57 -0800

    Also sort .csv

--- data-raw/make-dictionary.R
+++ data-raw/make-dictionary.R
@@ -4,5 +4,8 @@ rosetta_terms <- read.csv("inst/extdata/terms.csv", stringsAsFactors = FALSE,
 language <- "english"
 rosetta_terms <- rosetta_terms[order(rosetta_terms[[language]]), , drop = FALSE]
 
+write.csv(rosetta_terms, "inst/extdata/terms.csv", row.names = FALSE,
+  fileEncoding = "utf8")
+
 usethis::use_data(rosetta_terms, overwrite = TRUE, internal = TRUE)
 

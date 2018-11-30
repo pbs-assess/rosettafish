f05cdecc83ec1bf7861c6f76a3bad4bc72dcb614
Elise Keppel <elise.keppel@dfo-mpo.gc.ca>
2018-11-30 10:17:26 -0800

    Fix data input to sort function

--- R/sortfish.R
+++ R/sortfish.R
@@ -1,23 +1,22 @@
-# #' Title
-# #'
-# #' Sort fishrosetta input .csv file alphabetically for both
-# #'
-# #' @param language Language column in source terms.csv file to sort by. Either
-# #' "En" for English or "Fr" for French.
-# #'
-# #' @return Tibble ordered by specified language column.
-# #' @export
-# #'
-# #' @examples
-# #' terms_En <- sortfish("En")
-# #' terms_Fr <- sortfish("Fr")
-# sortfish <- function(language) {
-#   if (language == "En") {
-#   data <- dplyr::arrange(data, english)
-#   }
-#   if (language == "Fr") {
-#   data <- dplyr::arrange(data, french)
-#   }
-#   readr::write_csv(data, "data/terms.csv")
-# }
-# 
+#' Title
+#'
+#' Sort fishrosetta input .csv file alphabetically for both
+#'
+#' @param language Language column in source terms.csv file to sort by. Either
+#' "En" for English or "Fr" for French.
+#'
+#' @return Dataframe ordered by specified language column.
+#' @export
+#'
+#' @examples
+#' terms_En <- sort_lang("En")
+#' terms_Fr <- sort_lang("Fr")
+sort_lang <- function(language) {
+  if (language == "En") {
+  rosetta_terms <- dplyr::arrange(rosetta_terms, english)
+  }
+  if (language == "Fr") {
+  rosetta_terms <- dplyr::arrange(rosetta_terms, french)
+  }
+  readr::write_csv(rosetta_terms, "inst/extdata/terms.csv")
+}

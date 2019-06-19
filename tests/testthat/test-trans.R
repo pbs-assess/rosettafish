93203431428247ececc7d40415c67e18a5ba051d
Chris Grandin <cgrandin@shaw.ca>
2019-06-19 12:32:15 -0700

    Revert back to before trying to merge Maria's files in

--- tests/testthat/test-trans.R
+++ tests/testthat/test-trans.R
@@ -7,13 +7,6 @@ test_that("translate 3 from en to fr in data", {
                c("biomasse reproductrice", "mâle", "équipement"))
 })
 
-test_that("translate 3 from en to sp in data", {
-  expect_equal(trans(c("spawning biomass","male","equipment"),
-                     to = "french",
-                     from = "english"),
-               c("biomasse reproductrice", "mâle", "équipement"))
-})
-
 test_that("translate 3 from en to fr in data, one missing", {
   expect_equal(trans(c("spawning biomass","male","X"),
                      to = "french",
@@ -57,42 +50,9 @@ test_that("translate into 2 languages from en. French and inuktitut.", {
                "profondeur; ᐃᑎᔪᖅ")
 })
 
-test_that("give error on a word with no translation", {
-  expect_error(trans("missingterm",
-                     to = "french",
-                     from = "english",
-                     allow_missing = FALSE))
-})
-
-test_that("give error on multiple words with no translation", {
-  expect_error(trans(c("missingterm", "missingterm2"),
-                     to = "french",
-                     from = "english",
-                     allow_missing = FALSE))
-})
-
-test_that("give error on a word with no translation when other words have a match", {
-  expect_error(trans(c("missingterm", "depth"),
-                     to = "french",
-                     from = "english",
-                     allow_missing = FALSE))
-})
-
-test_that("give error on multiple words with no translation when other words have a match", {
-  expect_error(trans(c("missingterm", "missingterm2", "depth"),
-                     to = "french",
-                     from = "english",
-                     allow_missing = FALSE))
-})
-
-test_that("en2fr, en2sp, fr2en, and sp2en work as expected", {
+test_that("en2fr and fr2en work as expected", {
   expect_equal(en2fr("depth", TRUE), "profondeur")
   expect_equal(en2fr("depth", FALSE), "depth")
   expect_equal(fr2en("profondeur", TRUE), "depth")
   expect_equal(fr2en("profondeur", FALSE), "profondeur")
-
-  expect_equal(en2sp("Acadian Redfish", TRUE), "sébaste acadien")
-  expect_equal(en2sp("Acadian Redfish", FALSE), "Acadian Redfish")
-  expect_equal(sp2en("tieso pustuloso", TRUE), "Acned Snake Eel")
-  expect_equal(sp2en("tieso pustuloso", FALSE), "tieso pustuloso")
 })

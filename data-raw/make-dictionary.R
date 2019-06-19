faf243c838d5f689b19663c4771493b67cd5df4f
Elise Keppel <elise.keppel@dfo-mpo.gc.ca>
2019-06-19 13:36:47 -0700

    Add check for duplicates

--- data-raw/make-dictionary.R
+++ data-raw/make-dictionary.R
@@ -5,5 +5,17 @@ rosetta_terms <- rosetta_terms[order(rosetta_terms[[language]]), , drop = FALSE]
 row.names(rosetta_terms) <- NULL
 readr::write_csv(rosetta_terms, "inst/extdata/terms.csv")
 
+df <- rosetta_terms
+df$english <- tolower(df$english)
+df$french <- tolower(df$french)
+df$inuktitut <- tolower(df$inuktitut)
+
+#dups <- union(which(duplicated(df$english)), which(duplicated(df$french)))
+dups <- which(duplicated(df$english))
+
+if(length(dups>0)){
+  stop("The following lines have duplicates: ", paste(sort(dups) + 1, collapse = ", "))
+}
+
 usethis::use_data(rosetta_terms, overwrite = TRUE, internal = TRUE)
 

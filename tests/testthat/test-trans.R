d918d77b061b08dbf38b068926f8d803a0ebd38e
Sean Anderson <sean@seananderson.ca>
2019-06-24 10:47:42 -0700

    Fix checks

--- tests/testthat/test-trans.R
+++ tests/testthat/test-trans.R
@@ -1,17 +1,17 @@
 context("trans.R")
 
 test_that("translate 3 from en to fr in data", {
-  expect_equal(trans(c("spawning biomass","male","equipment"),
+  expect_equal(tolower(trans(c("Spawning biomass","Male","Equipment"),
                      to = "french",
-                     from = "english"),
+                     from = "english")),
                c("biomasse reproductrice", "mâle", "équipement"))
 })
 
 test_that("translate 3 from en to fr in data, one missing", {
-  expect_equal(trans(c("spawning biomass","male","X"),
+  expect_equal(tolower(trans(c("Spawning biomass","Male","X"),
                      to = "french",
-                     from = "english", allow_missing = TRUE),
-               c("biomasse reproductrice", "mâle", "X"))
+                     from = "english", allow_missing = TRUE)),
+               c("biomasse reproductrice", "mâle", "x"))
 })
 
 test_that("translate 3 from en to fr in data in data, all missing", {
@@ -22,16 +22,16 @@ test_that("translate 3 from en to fr in data in data, all missing", {
 })
 
 test_that("translate 3 from en to fr in data in data, with an NA", {
-  expect_equal(trans(c("spawning biomass","male", NA),
+  expect_equal(tolower(trans(c("Spawning biomass","Male", NA),
                      to = "french",
-                     from = "english", allow_missing = TRUE),
+                     from = "english", allow_missing = TRUE)),
                c("biomasse reproductrice", "mâle", NA))
 })
 
 test_that("translate into 2 languages from en", {
-  expect_equal(trans("male",
+  expect_equal(tolower(trans("Male",
                      to = c("english", "french"),
-                     from = "english", sep = "; "),
+                     from = "english", sep = "; ")),
       "male; mâle")
 })
 
@@ -44,15 +44,15 @@ test_that("translate into 2 languages from en, values don't exist in data", {
 
 
 test_that("translate into 2 languages from en. French and inuktitut.", {
-  expect_equal(trans(x = "depth",
+  expect_equal(tolower(trans(x = "Depth",
                      to = c("french", "inuktitut"),
-                     from = "english", sep = "; "),
+                     from = "english", sep = "; ")),
                "profondeur; ᐃᑎᔪᖅ")
 })
 
 test_that("en2fr and fr2en work as expected", {
-  expect_equal(en2fr("depth", TRUE), "profondeur")
-  expect_equal(en2fr("depth", FALSE), "depth")
-  expect_equal(fr2en("profondeur", TRUE), "depth")
-  expect_equal(fr2en("profondeur", FALSE), "profondeur")
+  expect_equal(en2fr("Depth", TRUE), "Profondeur")
+  expect_equal(en2fr("Depth", FALSE), "Depth")
+  expect_equal(fr2en("Profondeur", TRUE), "Depth")
+  expect_equal(fr2en("Profondeur", FALSE), "Profondeur")
 })
